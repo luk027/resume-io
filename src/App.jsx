765f9a689d9339ba184dad06e27e19b59a2281a3
Luk027 <lookman.gohil@gmail.com>
2025-03-14 18:26:38 +0530

    replace BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import GenerateResume from './pages/GenerateResume'
 import HomePage from './pages/HomePage'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import PageNotFound from './pages/PageNotFound'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={<HomePage />}/>
       <Route path='/createResume' element={<GenerateResume />}/>
       <Route path='*' element={<PageNotFound />}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
